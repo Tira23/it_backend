c063437ab3bb957e6ca655a5d25ddd8bf0cd7750
edanilkov <edanilkov@igatec.com>
2024-01-07 23:41:00 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+const app = express()
+const jsonMiddleware = express.json()
+app.use(cors())
+app.use(jsonMiddleware)
+const port = process.env.PORT || 3000
+let count = 0
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/count/:id', (req, res) => {
+    res.send('this page count: ' + req.params.id)
+})
+app.get('/count', (req, res) => {
+    res.send('this page count, and count now: ' + count)
+})
+
+app.post('/count', (req, res) => {
+    count = req.body.count
+    res.send('this page count, count update to: ' + count)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
