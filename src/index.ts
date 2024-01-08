64c31818d6feb832f5fd2336f547ad80bb2bd127
edanilkov <edanilkov@igatec.com>
2024-01-08 11:47:01 +0300

    count to array

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
-import express from 'express'
+import express, {response} from 'express'
 import cors from 'cors'
 const app = express()
 const jsonMiddleware = express.json()
 app.use(cors())
 app.use(jsonMiddleware)
 const port = process.env.PORT || 3000
-let count = [42]
+let arrayCounts = [{
+    id: 1,
+    count: 42
+} as {id: number, count: number}]
 app.get('/', (req, res) => {
     const html = `<ul>
-   ${count.map(item => `<li>   ${item}</li>`).join('')}
+   ${arrayCounts.map(item => `<li>   ${item.count}</li>`).join('')}
    </ul>
 `
     res.send(html)
@@ -17,14 +20,30 @@ app.get('/count/:id', (req, res) => {
     res.send('this page id: ' + req.params.id)
 })
 app.get('/count', (req, res) => {
-    res.send('this page count, and count now: ' + count.at(-1))
+    res.send('this page count, and count now: ' + arrayCounts.at(-1)?.count)
 })
 
-app.post('/count', (req, res) => {
-    count.push(req.body.count)
-    res.send('this page count, count update to: ' + count.at(-1))
+app.post('/count', (request, response) => {
+    if (typeof request.body.count !== 'number') {
+        response.status(422)
+        return
+    }
+    const obj = {
+        id: Date.now(),
+        count: request.body.count
+    }
+    arrayCounts.push(obj)
+    response.send('this page count, count update to: ' + arrayCounts.at(-1)?.count)
 })
 
+app.delete('/count', (request, response ) =>{
+    if (typeof request.body.count !== 'number') {
+        response.status(422)
+        return
+    }
+    arrayCounts = arrayCounts.filter(item => item.count !== request.body.count)
+    response.send(`all counts ${request.body.count} was delete`)
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
